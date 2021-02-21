6cd475de4e376ef313c0975824484882f08f3116
Yurii Donnikov <66084797+yurii-donnikov@users.noreply.github.com>
2021-02-21 22:37:35 +0200

    окончательный коммит

--- js/main.js
+++ js/main.js
@@ -1,24 +1,20 @@
-const animItems = document.querySelectorAll('._anim-items');
+// - скролл к 1/4 определенного объекта 
 
-console.log(animItems.length)
+const animItems = document.querySelectorAll('._anim-items');
 
 if (animItems.length > 0) {
   window.addEventListener('scroll', animOnScroll);
 
   function animOnScroll() {
-
     for (let index = 0; index < animItems.length; index++) {
-
       const animItem = animItems[index];
       const animItemHeight = animItem.offsetHeight; // - высота блока
       const animItemOffset = offset(animItem).top; // - высота от начала страницы к началу нашего блока
       const animStart = 4
-
       let animItemPoint = window.innerHeight - animItemHeight / animStart; // - высота 1/4 нашего блока при скролле вниз
       if (animItemHeight > window.innerHeight) {
         animItemPoint = window.innerHeight - window.innerHeight / animStart;
       }
-
       if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
         animItem.classList.add('active');
       } else {
@@ -36,12 +32,12 @@ if (animItems.length > 0) {
       left: rect.left + scrollLeft
     }
   }
-
 }
+
+//- отправда данных пользователя на сервер 
 
 const [submit, myPhone, myName, txtAfterInp] = ['submit', 'myPhone', 'myName', 'txtAfterInp']
 .map((id) => document.getElementById(id))
-
 submit.onclick = function (event) {
   if (myPhone.value.length > 4) {
     fetch('http://localhost:3000/posts', {
@@ -60,4 +56,59 @@ submit.onclick = function (event) {
   } else {
     txtAfterInp.innerHTML = 'введите корректый номер'
   }
+}
+
+//- модальное окно под гугл-карту
+
+const btnone = document.querySelector('.btnone')
+const close = document.querySelector('.close')
+const mapContent = document.querySelector('.mapContent')
+
+btnone.onclick = function () {
+  mapContent.style.display = 'block';
+  document.getElementsByTagName('header')[0].style.opacity = 0.8;
+  document.getElementsByTagName('main')[0].style.opacity = 0.8;
+  document.getElementsByTagName('footer')[0].style.opacity = 0.8;
+}
+close.onclick = function () {
+  mapContent.style.display = 'none'
+  document.getElementsByTagName('header')[0].style.opacity = 1;
+  document.getElementsByTagName('main')[0].style.opacity = 1;
+  document.getElementsByTagName('footer')[0].style.opacity = 1;
+}
+window.onclick = function (event) {
+  if (event.target == mapContent) {
+    mapContent.style.display = 'none'
+    document.getElementsByTagName('header')[0].style.opacity = 1;
+    document.getElementsByTagName('main')[0].style.opacity = 1;
+    document.getElementsByTagName('footer')[0].style.opacity = 1;
+  }
+}
+
+var map;
+// Функция initMap которая отрисует карту на странице
+function initMap() {
+  // В переменной map создаем объект карты GoogleMaps и вешаем эту переменную на <div id="map"></div>
+  map = new google.maps.Map(document.getElementById('map'), {
+    // При создании объекта карты необходимо указать его свойства
+    // center - определяем точку на которой карта будет центрироваться
+    center: {
+      lat: 49.99271443804456,
+      lng: 36.23078124675806
+    },
+    // zoom - определяет масштаб. 0 - видно всю платнеу. 18 - видно дома и улицы города.
+    zoom: 15
+  });
+  // Создаем маркер на карте
+  var marker = new google.maps.Marker({
+    // Определяем позицию маркера
+    position: {
+      lat: 49.99271443804456,
+      lng: 36.23078124675806
+    },
+    // Указываем на какой карте он должен появится. (На странице ведь может быть больше одной карты)
+    map: map,
+    // Пишем название маркера - появится если навести на него курсор и немного подождать
+    title: 'BORODINSKI'
+  });
 }
